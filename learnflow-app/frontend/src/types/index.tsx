db387eefab193d8fc5d7eb927d6070b40008b5e6
Muahmmad Mustafa <mustafa1212adnan@gmail.com>
2026-06-18 01:36:45 +0500

    Update index.tsx

--- learnflow-app/frontend/src/types/index.tsx
+++ learnflow-app/frontend/src/types/index.tsx
@@ -24,3 +24,14 @@ export const MASTERY_LABELS = {
   PROFICIENT: "Proficient",
   EXPERT: "Expert",
 };
+
+export const PYTHON_CURRICULUM = [
+  { id: 1, name: "Basics", topics: ["Variables", "Data Types", "Input/Output", "Operators", "Type Conversion"] },
+  { id: 2, name: "Control Flow", topics: ["Conditionals", "For Loops", "While Loops", "Break/Continue"] },
+  { id: 3, name: "Data Structures", topics: ["Lists", "Tuples", "Dictionaries", "Sets"] },
+  { id: 4, name: "Functions", topics: ["Defining Functions", "Parameters", "Return Values", "Scope"] },
+  { id: 5, name: "OOP", topics: ["Classes & Objects", "Attributes & Methods", "Inheritance", "Encapsulation"] },
+  { id: 6, name: "Files", topics: ["Reading/Writing Files", "CSV Processing", "JSON Handling"] },
+  { id: 7, name: "Errors", topics: ["Try/Except", "Exception Types", "Custom Exceptions", "Debugging"] },
+  { id: 8, name: "Libraries", topics: ["Installing Packages", "Working with APIs", "Virtual Environments"] },
+];
